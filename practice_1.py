00a021f040022a4574374d1eb81864b6322cb1a4
WilfriedVanDyk <wilfriedvandyk@gmail.com>
2022-04-23 21:20:41 +0200

    tryout pytest
    adding test
    
    added 1 practice page
    
    organsing diff test pages
    
    add 2 practice files and 2 corresponding test files

---
+++ practice_1.py
@@ -0,0 +1,11 @@
+""" practice page 2"""
+
+
+"""make a dog class and use inheretance"""
+class dog:
+    """making a concrete dog
+    """
+    def __init__(self, name_of_dog):
+        self.name = name_of_dog
+
+"""builder pattern"""
